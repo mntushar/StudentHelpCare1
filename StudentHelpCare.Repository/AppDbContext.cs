0fe1f8fbb9c958b84e8999b5a12bd55b945991c2
M N Tushar <mntushar25@gmail.com>
2023-01-18 14:02:14 +0600

    add identity

--- StudentHelpCare.Repository/AppDbContext.cs
+++ StudentHelpCare.Repository/AppDbContext.cs
@@ -1,15 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using StudentHelpCare.Data.Entity;
 
 namespace StudentHelpCare.Repository
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
+        public DbSet<Role> Role { get; set; }
+        public DbSet<StudentEntity> Student { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
         {
         }
 
-        public DbSet<StudentEntity> Student { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUser>().ToTable("User");
+            builder.Entity<IdentityRole>().ToTable("Role");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserToken");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRole");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("UserRoleClaim");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClim");
+        }
     }
 }
--- StudentHelpCare/Program.cs
+++ StudentHelpCare/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using StudentHelpCare.AppSetting;
 using StudentHelpCare.Repository;
@@ -7,11 +8,47 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;
+});
+
 //dependency injection
 builder.Services.RegisterRepository();
 builder.Services.RegisterServices();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 //register maps
 app.RegisterMap();
