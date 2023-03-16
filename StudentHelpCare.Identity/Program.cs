179401ad6141e05105cadbd97e218a0ab1ca6c3e
M N Tushar <mntushar25@gmail.com>
2023-03-16 08:51:02 +0600

    add Swagger identity server

--- StudentHelpCare.Identity/Program.cs
+++ StudentHelpCare.Identity/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using StudentHelpCare.Identity.Repository;
+using StudentHelpCare.StudentHelpCare.AppSetting;
 using StudentHelpCare.StudentHelpCareIdentityServer.AppSetting;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +9,37 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+//third party library
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//dependency injection
+builder.Services.RegisterRepository();
+builder.Services.RegisterServices();
+
 //middelware
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 //register maps
 app.RegisterMap();
