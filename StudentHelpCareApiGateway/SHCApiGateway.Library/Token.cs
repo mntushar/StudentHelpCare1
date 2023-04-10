072ded4f6a23c5444c2dd1b5666ec3b6e53462bc
M N Tushar <mntushar25@gmail.com>
2023-04-10 18:15:29 +0600

    update token

--- StudentHelpCareApiGateway/SHCApiGateway.Library/Token.cs
+++ StudentHelpCareApiGateway/SHCApiGateway.Library/Token.cs
@@ -8,13 +8,14 @@ namespace SHCApiGateway.Library
 {
     public static class Token
     {
-        public static string CreateJWTSymmetricToken(User user, string secretKey)
+        public static string CreateJWTSymmetricToken(User user, string secretKey, DateTime tokenValidationTime,
+            string algorithom, string issuer, string audience)
         {
             // Define your symmetric key
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
 
             // Create signing credentials using the key
-            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.RsaSha256);
+            var signingCredentials = new SigningCredentials(securityKey, algorithom);
 
             // Create some claims to include in the token
             var claims = new[]
@@ -25,10 +26,10 @@ namespace SHCApiGateway.Library
 
             // Define the token's options
             var tokenOptions = new JwtSecurityToken(
-                issuer: "your_issuer_here",
-                audience: "your_audience_here",
+                issuer: issuer,
+                audience: audience,
                 claims: claims,
-                expires: DateTime.UtcNow.AddDays(1),
+                expires: tokenValidationTime,
                 signingCredentials: signingCredentials
             );
 
