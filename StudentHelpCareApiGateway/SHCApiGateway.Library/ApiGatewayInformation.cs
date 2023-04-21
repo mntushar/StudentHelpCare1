853ac9359e7ad59180d02bade575fea3f016ef04
M N Tushar <mntushar25@gmail.com>
2023-04-21 10:17:03 +0600

    create ras key

--- StudentHelpCareApiGateway/SHCApiGateway.Library/ApiGatewayInformation.cs
+++ StudentHelpCareApiGateway/SHCApiGateway.Library/ApiGatewayInformation.cs
@@ -1,38 +1,7 @@
-﻿using System.Security.Cryptography;
-using System.Text;
-
-namespace SHCApiGateway.Library
+﻿namespace SHCApiGateway.Library
 {
     public class ApiGatewayInformation
     {
-        private static readonly string _jwtSymmetricTokenKry = "test1test";
         public static readonly string url = "https://localhost:5266";
-
-        public static string JwtSymmetricTokenKey()
-        {
-            string key = string.Empty;
-
-            try
-            {
-                byte[]? byteArray = Encoding.UTF8.GetBytes(_jwtSymmetricTokenKry);
-
-                if (byteArray == null) return key;
-
-                using (var hmac = new HMACSHA256(byteArray))
-                {
-                    byte[] byteKey = hmac.ComputeHash(byteArray);
-                    byte[] truncatedKey = new byte[16];
-                    Array.Copy(byteKey, truncatedKey, 16);
-
-                    key = Convert.ToBase64String(truncatedKey);
-                }
-            }
-            catch (Exception ex) 
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            return key;
-        }
     }
 }
--- StudentHelpCareApiGateway/SHCApiGateway.Library/Token.cs
+++ StudentHelpCareApiGateway/SHCApiGateway.Library/Token.cs
@@ -3,6 +3,7 @@ using Microsoft.IdentityModel.Tokens;
 using SHCApiGateway.Data.Entity;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace SHCApiGateway.Library
@@ -13,6 +14,90 @@ namespace SHCApiGateway.Library
         LoggerFactory.Create(builder =>
             builder.AddConsole())
             .CreateLogger(typeof(Token));
+        private static readonly RSAParameters _rsakeyParams = new RSAParameters
+        {
+            Modulus = Encoding.ASCII.GetBytes("my-custom-key-modulus"),
+            Exponent = Encoding.ASCII.GetBytes("my-custom-key-exponent"),
+            D = Encoding.ASCII.GetBytes("my-custom-key-d"),
+            DP = Encoding.ASCII.GetBytes("my-custom-key-dp"),
+            DQ = Encoding.ASCII.GetBytes("my-custom-key-dq"),
+            InverseQ = Encoding.ASCII.GetBytes("my-custom-key-inverseq"),
+            P = Encoding.ASCII.GetBytes("my-custom-key-p"),
+            Q = Encoding.ASCII.GetBytes("my-custom-key-q")
+        };
+        private static readonly int _rsaKeySize = 2048;
+        private static readonly string _SymmetricSecretKry = "test1test";
+
+        public static string SymmetricKey()
+        {
+            string key = string.Empty;
+
+            try
+            {
+                byte[]? byteArray = Encoding.UTF8.GetBytes(_SymmetricSecretKry);
+
+                if (byteArray == null) return key;
+
+                using (var hmac = new HMACSHA256(byteArray))
+                {
+                    byte[] byteKey = hmac.ComputeHash(byteArray);
+                    byte[] truncatedKey = new byte[16];
+                    Array.Copy(byteKey, truncatedKey, 16);
+
+                    key = Convert.ToBase64String(truncatedKey);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return key;
+        }
+
+        public static RSAParameters? AsymmetricPrivateKey()
+        {
+            RSAParameters? privateKey = null;
+
+            try
+            {
+                using (RSA rsa = RSA.Create(_rsaKeySize))
+                {
+                    rsa.ImportParameters(_rsakeyParams);
+
+                    // Get the public and private keys
+                    privateKey = rsa.ExportParameters(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return privateKey;
+        }
+
+        public static RSAParameters? AsymmetricPublicKey()
+        {
+            RSAParameters? publicKey = null;
+
+            try
+            {
+                using (RSA rsa = RSA.Create(_rsaKeySize))
+                {
+                    rsa.ImportParameters(_rsakeyParams);
+
+                    // Get the public and private keys
+                    publicKey = rsa.ExportParameters(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return publicKey;
+        }
 
         public static string GenerateJWTSymmetricToken(Claim[] claims,
             string secretKey, DateTime tokenValidationTime,
@@ -27,6 +112,37 @@ namespace SHCApiGateway.Library
 
                 // Create signing credentials using the key
                 var signingCredentials = new SigningCredentials(securityKey, algorithom);
+
+                // Define the token's options
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: issuer,
+                    audience: audience,
+                    claims: claims,
+                    expires: tokenValidationTime,
+                    signingCredentials: signingCredentials
+                );
+
+                // Generate the token as a string
+                tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            }
+            catch (Exception ex)
+            {
+                _Logger.LogError(ex, "Token Error", ex);
+            }
+
+            return tokenString;
+        }
+
+        public static string GenerateJWTAsymmetricToken(Claim[] claims,
+           RSAParameters privateKey, RSAParameters? publicKey, DateTime tokenValidationTime,
+           string algorithom, string issuer, string audience)
+        {
+            string tokenString = string.Empty;
+
+            try
+            {
+                // Create signing credentials using the key
+                var signingCredentials = new SigningCredentials(new RsaSecurityKey(privateKey), SecurityAlgorithms.RsaSha256);
 
                 // Define the token's options
                 var tokenOptions = new JwtSecurityToken(
@@ -70,7 +186,7 @@ namespace SHCApiGateway.Library
                     new Claim("iat", new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds().ToString()),
                 };
 
-                token = GenerateJWTSymmetricToken(clims, ApiGatewayInformation.JwtSymmetricTokenKey(),
+                token = GenerateJWTSymmetricToken(clims, SymmetricKey(),
                                 DateTime.Now.AddDays(1), SecurityAlgorithms.HmacSha256, ApiGatewayInformation.url,
                                 ApiGatewayInformation.url);
                 }
